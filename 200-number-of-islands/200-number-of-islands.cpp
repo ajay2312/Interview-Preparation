7b22620fba7912b0d737e7fca3c8e76bd79927a3
Ajay Kumar Dangi <47072129+ajay2312@users.noreply.github.com>
2022-01-25 22:13:54 +0530

    Time: 38 ms (57.44%), Space: 12.3 MB (59.08%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    void find(vector<vector<char>> &grid,int n,int m,int i,int j){
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0')
+            return;
+        grid[i][j]='0';
+        find(grid,n,m,i+1,j);
+        find(grid,n,m,i,j+1);
+        find(grid,n,m,i-1,j);
+        find(grid,n,m,i,j-1);
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int cnt=0;
+        int n=grid.size(),m=grid[0].size();
+        // vector<vector<int>> visited(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    find(grid,n,m,i,j);
+                }
+            }
+        }
+        return cnt;
+    }
+};
