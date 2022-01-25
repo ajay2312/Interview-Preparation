cd00b32b0e04351bcc9ffcef339fc24390f3d670
Ajay Kumar Dangi <47072129+ajay2312@users.noreply.github.com>
2022-01-25 22:33:27 +0530

    Time: 36 ms (69.30%), Space: 13.4 MB (29.78%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     
-    void find(vector<vector<char>> &grid,int n,int m,int i,int j){
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0')
+    void find(vector<vector<char>> &grid,int n,int m,int i,int j,vector<vector<int>> &visited){
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0' || visited[i][j])
             return;
-        grid[i][j]='0';
-        find(grid,n,m,i+1,j);
-        find(grid,n,m,i,j+1);
-        find(grid,n,m,i-1,j);
-        find(grid,n,m,i,j-1);
+        visited[i][j]=1;
+        find(grid,n,m,i+1,j,visited);
+        find(grid,n,m,i,j+1,visited);
+        find(grid,n,m,i-1,j,visited);
+        find(grid,n,m,i,j-1,visited);
     }
     
     int numIslands(vector<vector<char>>& grid) {
         int cnt=0;
         int n=grid.size(),m=grid[0].size();
-        // vector<vector<int>> visited(n,vector<int>(m,0));
+        vector<vector<int>> visited(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
+                if(grid[i][j]=='1' && !visited[i][j]){
                     cnt++;
-                    find(grid,n,m,i,j);
+                    find(grid,n,m,i,j,visited);
                 }
             }
         }
